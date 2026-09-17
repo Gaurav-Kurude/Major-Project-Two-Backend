9f49075450b8a98d657bcb9f66d7890e50333d3b
Gaurav-Kurude <gauravkurude.sitmech@gmail.com>
2026-09-17 15:24:12 +0530

    Major Project Two Backend

--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getAllTags } = require("../controllers/tagControllers");
+const { createTag,getAllTags } = require("../controllers/tagControllers");
 
 router.post("/", createTag);
 router.get("/", getAllTags);
